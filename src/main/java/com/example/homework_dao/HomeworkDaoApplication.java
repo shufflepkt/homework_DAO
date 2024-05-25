e8ef4152c821886e4e0ca47d1a9223d1ba754c4c
Yury Shakhmatov <shufflepkt@yandex.ru>
2024-05-25 14:51:57 +0200

    Initial commit

---
+++ src/main/java/com/example/homework_dao/HomeworkDaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework_dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkDaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkDaoApplication.class, args);
+    }
+
+}
