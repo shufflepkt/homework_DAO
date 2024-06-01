cc58b9a9eb75d297955840ec6df76b55280624d7
Yury Shakhmatov <shufflepkt@yandex.ru>
2024-06-01 21:04:17 +0200

    work in process 3

--- src/main/resources/db.changelog/migrations/customers.sql
+++ src/main/resources/db.changelog/migrations/customers.sql
@@ -1,17 +1,17 @@
 insert into homework.customers_1 (id, name, surname, age, phone_number)
-values(nextval('hibernate_sequence'), 'Yury', 'Shakhmatov', 39, '+79999999999');
+values(1, 'Yury', 'Shakhmatov', 39, '+79999999999');
 
-insert into homework.customers_1 (name, surname, age, phone_number)
-values('Dmitry', 'Stepanov', 30, '+78888888888');
+insert into homework.customers_1 (id, name, surname, age, phone_number)
+values(2, 'Dmitry', 'Stepanov', 30, '+78888888888');
 
-insert into homework.customers_1 (name, surname, age, phone_number)
-values('Alex', 'Ivanov', 25, '+79056666666');
+insert into homework.customers_1 (id, name, surname, age, phone_number)
+values(3, 'Alex', 'Ivanov', 25, '+79056666666');
 
-insert into homework.customers_1 (name, surname, age, phone_number)
-values('Alexey', 'Pertov', 21, '+79079898989');
+insert into homework.customers_1 (id, name, surname, age, phone_number)
+values(4, 'Alexey', 'Pertov', 21, '+79079898989');
 
-insert into homework.customers_1 (name, surname, age, phone_number)
-values('alexey', 'Sidorov', 45, '+79170000001');
+insert into homework.customers_1 (id, name, surname, age, phone_number)
+values(5, 'alexey', 'Sidorov', 45, '+79170000001');
 
-insert into homework.customers_1 (name, surname, age, phone_number)
-values('ALEXEY', 'Platonov', 19, '+79250000007');
+insert into homework.customers_1 (id, name, surname, age, phone_number)
+values(6, 'ALEXEY', 'Platonov', 19, '+79250000007');
--- src/main/resources/db.changelog/migrations/orders.sql
+++ src/main/resources/db.changelog/migrations/orders.sql
@@ -1,14 +1,14 @@
-insert into homework.orders_1 (date, customer_id, product_name, amount)
-values ('2024-05-19', 1, 'Samsung TV', 1);
+insert into homework.orders_1 (id, date, customer_id, product_name, amount)
+values (1, '2024-05-19', 1, 'Samsung TV', 1);
 
-insert into homework.orders_1 (date, customer_id, product_name, amount)
-values ('2023-12-10', 2, 'Laptop Lenovo', 2);
+insert into homework.orders_1 (id, date, customer_id, product_name, amount)
+values (2, '2023-12-10', 2, 'Laptop Lenovo', 2);
 
-insert into homework.orders_1 (date, customer_id, product_name, amount)
-values ('2024-03-01', 3, 'Router MX', 3);
+insert into homework.orders_1 (id, date, customer_id, product_name, amount)
+values (3, '2024-03-01', 3, 'Router MX', 3);
 
-insert into homework.orders_1 (date, customer_id, product_name, amount)
-values ('2024-04-14', 4, 'PlayStation-6', 4);
+insert into homework.orders_1 (id, date, customer_id, product_name, amount)
+values (4, '2024-04-14', 4, 'PlayStation-6', 4);
 
-insert into homework.orders_1 (date, customer_id, product_name, amount)
-values ('2024-01-22', 5, 'iPhone 12Pro', 5);
+insert into homework.orders_1 (id, date, customer_id, product_name, amount)
+values (5, '2024-01-22', 5, 'iPhone 12Pro', 5);
